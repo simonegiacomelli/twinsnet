0ab7f5d347bcf1703a408c19449a9afda1fe71f1
Simone <simone.giacomelli@gmail.com>
2022-04-01 09:54:38 +0200

    Add project files.

---
+++ TwinsnetConsole/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+Console.WriteLine("Hello, World!");
+
+string str1 = "123";
+int int1 = 100;
+
+int1 = int.Parse(str1);
+
+str1 = int1.ToString();
+
+string str2 = int1.ToString("#.0");
+
+Console.WriteLine(str2);
+char c = 'x';
+
+Console.WriteLine(str1 + " " + int1);
+
+DateTime dt1 = DateTime.Now;
+
+Console.WriteLine(dt1);
+
+
+int int2 = 456;
+
+////
+var int3 = 789;
+
+int3 = "ciao";
+
+var int4 = str2.Split(",");
+
+
+
+
