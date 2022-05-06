2d3c0808caba2ee752e030aad4c430bcc8c0fe48
Simone <simone.giacomelli@gmail.com>
2022-05-06 12:35:51 +0200

    cli, email, web server

---
+++ TwinsnetConsole/Cli.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwinsnetConsole
+{
+    internal class Cli
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine($"\nI'm {typeof(Cli)}");
+            Console.WriteLine($"Numero arogmenti: {args.Length}");
+            foreach (var arg in args)
+            {
+                Console.WriteLine(arg);
+            }
+
+            Console.WriteLine();
+
+            var cliArgs = new CliArguments(args);
+            cliArgs.Parse();
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine($"opzione File={cliArgs.File}");
+            Console.WriteLine($"opzione Content={cliArgs.Content}");
+
+            // cliArgs.Content = " bla bla "; e' privato, non posso accedervi
+            
+            if (cliArgs.File.Length > 0)
+                File.AppendAllText(cliArgs.File, cliArgs.Content);
+
+            Console.WriteLine($"cliArgs.NumeroArguments={cliArgs.NumeroArguments}");
+
+
+        }
+
+    }
+
+    public class CliArguments
+    {
+        private string[] args;
+
+        public string File { get; private set; } = "";
+
+        public string Content { get; private set; } = "";
+
+
+        public int NumeroArguments
+        {
+            get
+            {
+                return args.Length;
+            }
+        }
+
+        public CliArguments(string[] args)
+        {
+            this.args = args;
+        }
+
+        public void Parse()
+        {
+            for (int idx = 0; idx < args.Length; idx++)
+            {
+                var arg = args[idx];
+                var argSuccessivo = "";
+                if (idx < (args.Length - 1))
+                    argSuccessivo = args[idx + 1];
+
+                if (arg.StartsWith("-"))
+                    ProcessArgument(arg, argSuccessivo);
+            }
+        }
+
+        private void ProcessArgument(string arg, string argSuccessivo)
+        {
+            var opt = arg.Substring(1).ToLowerInvariant();
+            Console.WriteLine($"{opt}={argSuccessivo}");
+            switch (opt)
+            {
+                case "file":
+                    File = argSuccessivo;
+                    break;
+                case "content":
+                    Content = argSuccessivo;
+                    break;
+
+            }
+
+        }
+    }
+}
---
+++ TwinsnetConsole/Email.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwinsnetConsole
+{
+    internal class Email
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine($"I'm {nameof(Email)}");
+
+            if (!File.Exists("password.txt"))
+                throw new Exception("Manca il file password.txt");
+
+            var password = File.ReadAllText("password.txt");
+
+            // MailMessage e' la classe che conosce e manipola il file di test (.eml) di una email
+            MailMessage Msg = new MailMessage();
+            Msg.From = new MailAddress("fests@simone.pro", "Fests Giacomelli");
+            Msg.To.Add("simone.giacomelli@gmail.com");
+            Msg.Subject = $"twinsnet console {DateTime.Now}";
+            Msg.Body = "body1\n123456789012345678901234567890\n\nCiao da Fests.";
+            Msg.ReplyToList.Add(new MailAddress("info@simone.pro", "Info Giacomelli"));
+            Msg.Attachments.Add(new Attachment("logo.png"));
+
+
+            // fix sicurezza gmail 
+            SmtpClient smtp = new SmtpClient();
+            smtp.Host = "smtp.gmail.com";
+            smtp.Port = 587;
+            smtp.Credentials = new System.Net.NetworkCredential("fests@simone.pro", password );
+            smtp.EnableSsl = true;
+            smtp.Send(Msg);
+        }
+    }
+}
