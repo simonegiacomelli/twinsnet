05e11f4191f3df35b5edfdffc0a2507c3b04b338
Simone <simone.giacomelli@gmail.com>
2022-05-06 13:07:03 +0200

    webserver

---
+++ TwinsnetConsole/WebServer.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwinsnetConsole
+{
+    internal class WebServer
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine($"I'm {nameof(WebServer)}");
+            
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => $"Hello World! {DateTime.Now}");
+
+            app.MapGet("/scrivi", () =>
+            {
+
+                File.AppendAllText("webserver-scrivi.txt", "ciao\n");
+                return File.ReadAllText("webserver-scrivi.txt");
+            });
+
+            app.Run();
+        }
+    }
+}
