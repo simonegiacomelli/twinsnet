9d5e929836dcf91964905eb15587675e61ffedb5
Simone <simone.giacomelli@gmail.com>
2022-05-20 10:22:00 +0200

    interfacce e ordinamento

---
+++ TwinsnetConsole/Ordinamento.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwinsnetConsole;
+
+class Ordinamento
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine($"I'm {nameof(Ordinamento)}");
+
+        var dispositivi = new List<Dispositivo> {
+            new Dispositivo { Id=1, Descrizione ="Motore avvio", Tipologia = "motore" },
+            new Dispositivo { Id=2, Descrizione = "Rele avvio", Tipologia = "rele" },
+            new Dispositivo { Id=3, Descrizione ="Pompa spurgo", Tipologia = "pompa" } ,
+            new Dispositivo { Id=4, Descrizione = "Motore girello", Tipologia= "motore" },
+        };
+
+        // fluent api
+        
+        var o1 = dispositivi
+            .OrderBy(x => x.Tipologia)
+            .ThenBy(x => x.Descrizione)
+            .ToList();
+
+        foreach(var dispositivo in o1)
+        {
+            Console.WriteLine(dispositivo);
+        }
+
+    }
+}
+
+//poco plain old c# object
+
+class Dispositivo
+{
+    public int Id { get; set; }
+    public string Descrizione { get; set; }
+    public string Tipologia { get; set; }
+
+ 
+    override public string ToString()
+    {
+        return $"{Id} {Descrizione.PadRight(25)} {Tipologia}";
+    }
+}
---
+++ TwinsnetConsole/oop/Interfaccia.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwinsnetConsole.oop;
+
+interface Serbatoio
+{
+    int Disponibilita { get; set; }
+
+
+    // int capacita;  non si puo' fare perche' un interfaccia non ha fields
+    // int Capacita(); questo e' valido ma preferisco usare le proprieta'
+    int Capacita { get; }
+
+}
+
+interface Motore
+{
+    int NumeroCilindri { get; }
+    bool Avvia();
+
+
+}
+
+
+
+class Trattore : Serbatoio, Motore
+{
+    public int Disponibilita { get ; set; }
+
+    public int Capacita => 80;
+
+    public int NumeroCilindri => 6;
+
+    public bool Avvia()
+    {
+        throw new NotImplementedException();
+    }
+}
